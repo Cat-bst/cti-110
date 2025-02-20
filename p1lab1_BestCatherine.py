c8aec1e5cff9243628735dddc3ab1c713448efae
Cat-bst <bestc7959@student.faytechcc.edu>
2025-02-20 15:14:14 -0500

    Add files via upload

---
+++ p1lab1_BestCatherine.py
@@ -0,0 +1,10 @@
+# Cat Best
+# 2/20/2025
+# P1LAB1
+# Welcoming the user
+
+first_name = input("Please enter your first name: ")
+
+last_name = input("Please enter your last name: ")
+
+print(f"Welcome to CTI-110, {first_name} {last_name}!")
